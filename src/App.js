64cc36d2258a8bbd7d3088f856bb6f13b3f56c47
Pete Schroeder <Pete.Schroeder@logmein.com>
2022-03-07 16:43:54 -0500

    Rework
    DELETE>>>DELETE

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+function App() {
+    return (<div>
+        <h2>Welcome to React App w/Webpack</h2>
+        <h3>Date : {new Date().toDateString()}</h3>
+    </div>)
+}
+
+export default App
